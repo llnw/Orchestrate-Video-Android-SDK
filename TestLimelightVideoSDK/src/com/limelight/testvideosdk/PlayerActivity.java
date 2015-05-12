3a0628f92a42e27bcd1cf412ae8c827ee37888a6
kanchankumar1 <kanchan.kumar@rebaca.com>
2015-05-12 15:36:30 +0530

    Code Clean up
    PlayerActivity1 renamed to playerActivity and old playerActivity is
    deleted as it is no more required.

--- TestLimelightVideoSDK/src/com/limelight/testvideosdk/PlayerActivity.java
+++ TestLimelightVideoSDK/src/com/limelight/testvideosdk/PlayerActivity.java
@@ -1,27 +1,37 @@
 package com.limelight.testvideosdk;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Color;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.util.SparseArray;
+import android.webkit.URLUtil;
+import com.limelight.testvideosdk.ChannelFragment.ChannelCallback;
+import com.limelight.testvideosdk.ChannelGroupFragment.ChannelGroupCallback;
+import com.limelight.testvideosdk.MediaFragment.MediaCallback;
+import com.limelight.testvideosdk.SpecificChannelFragment.SpecificChannelCallback;
+import com.limelight.testvideosdk.SpecificChannelGroupFragment.SpecificChannelGroupCallback;
 import com.limelight.videosdk.Constants;
+import com.limelight.videosdk.Constants.PlayerState;
+import com.limelight.videosdk.ContentService;
 import com.limelight.videosdk.IPlayerCallback;
 import com.limelight.videosdk.IPlayerControl;
-import com.limelight.videosdk.Constants.PlayerState;
+import com.limelight.videosdk.model.Media;
 
-public class PlayerActivity extends FragmentActivity implements
-        IPlayerCallback, OnPageChangeListener {
+public class PlayerActivity extends FragmentActivity implements IPlayerCallback, OnPageChangeListener,ActionBar.TabListener {
 
     private static final int READ_REQUEST_CODE = 50;
     private ViewPager mViewPager;
@@ -29,35 +39,159 @@ public class PlayerActivity extends FragmentActivity implements
     private IPlayerControl mControl;
     private PlayersFragment mPlayerFragment = null;
     private SparseArray<Fragment> mRefer = new SparseArray<Fragment>();
+    private PlayerTestAdapter mPlayerTestAdapter;
 
-    public void onCreate(Bundle savedInstanceState) {
+    private ChannelFragment mChannelFragment;
+    private MediaFragment mMediaFragment;
+    private SpecificChannelGroupFragment mSpecificChannelGroupFragment;
+    private SpecificChannelFragment mSpecificChannelFragment;
+    private String mGroupId = null;
+    private String mChannelId = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_main);
-        PlayerTestAdapter mPlayerTestAdapter = new PlayerTestAdapter(getSupportFragmentManager());
+        setContentView(R.layout.fragment_main1);
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mPlayerTestAdapter = new PlayerTestAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
-        PagerTabStrip strip = (PagerTabStrip)findViewById(R.id.pager_title_strip);
-        strip.setDrawFullUnderline(true);
-        strip.setTabIndicatorColor(Color.BLUE);
-        mViewPager.setAdapter(mPlayerTestAdapter);
         mViewPager.setOnPageChangeListener(this);
-        mViewPager.setOffscreenPageLimit(2);
+        mViewPager.setAdapter(mPlayerTestAdapter);
+        mViewPager.setOffscreenPageLimit(7);
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(0)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(1)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(2)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(3)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(4)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(5)).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(mPlayerTestAdapter.getPageTitle(6)).setTabListener(this));
     }
 
+    ChannelGroupCallback channelGroupCallback = new ChannelGroupCallback() {
+        @Override
+        public void callback(String id) {
+            mGroupId = id;
+            if(mSpecificChannelGroupFragment != null)
+                mSpecificChannelGroupFragment.restartLoader(id);
+            mViewPager.setCurrentItem(3);
+        }
+    };
+
+    ChannelCallback channelCallback = new ChannelCallback() {
+        @Override
+        public void callback(String id) {
+            mChannelId = id;
+            if(mSpecificChannelFragment != null)
+                mSpecificChannelFragment.restartLoader(id);
+            mViewPager.setCurrentItem(5);
+        }
+
+        @Override
+        public void playChannel(String channelId) {
+            if(mPlayerFragment != null){
+                mPlayerFragment.playChannel(channelId);
+            }
+            mViewPager.setCurrentItem(6);
+        }
+    };
+
+    SpecificChannelGroupCallback specificChannelGroupCallback = new SpecificChannelGroupCallback() {
+        @Override
+        public void callback(String id) {
+            mChannelId = id;
+            if(mSpecificChannelFragment != null)
+                mSpecificChannelFragment.restartLoader(id);
+            mViewPager.setCurrentItem(5);
+        }
+    };
+    
+    MediaCallback mediaCallback = new MediaCallback() {
+        @Override
+        public void callback(String id, ContentService svc) {
+            mChannelId = id;
+            if(mPlayerFragment != null){
+                mPlayerFragment.setEditText(id);
+                mPlayerFragment.play(svc);
+            }
+            mViewPager.setCurrentItem(6);
+        }
+
+        @Override
+        public void addToPlaylist(Media media) {
+            if(mPlayerFragment != null){
+                mPlayerFragment.addToPlaylist(media);
+            }
+        }
+
+        @Override
+        public void removeFromPlaylist(Media media) {
+            if(mPlayerFragment != null){
+                mPlayerFragment.removeFromPlaylist(media);
+            }
+        }
+    };
+    SpecificChannelCallback mediaCallback1 = new SpecificChannelCallback() {
+        @Override
+        public void callback(String id, ContentService svc) {
+            mChannelId = id;
+            if(mPlayerFragment != null){
+                mPlayerFragment.setEditText(id);
+                mPlayerFragment.play(svc);
+            }
+            mViewPager.setCurrentItem(6);
+        }
+
+        @Override
+        public void addToPlaylist(Media media) {
+            if(mPlayerFragment != null){
+                mPlayerFragment.addToPlaylist(media);
+            }
+        }
+
+        @Override
+        public void removeFromPlaylist(Media media) {
+            if(mPlayerFragment != null){
+                mPlayerFragment.removeFromPlaylist(media);
+            }
+        }
+    };
+
     public class PlayerTestAdapter extends FragmentPagerAdapter {
+        int count = 7;
         public PlayerTestAdapter(FragmentManager fm) {
             super(fm);
+        }
+
+        public void setCount(int val) {
+            count = val;
         }
 
         @Override
         public Fragment getItem(int i) {
             switch (i) {
             case 0:
+                SettingsFragment settingFragment = new SettingsFragment();
+                return settingFragment;
+            case 1:
+                ChannelGroupFragment channelGroupFragment = new ChannelGroupFragment(channelGroupCallback);
+                return channelGroupFragment;
+            case 2:
+                mChannelFragment = new ChannelFragment(channelCallback);
+                return mChannelFragment;
+            case 3:
+                mSpecificChannelGroupFragment = new SpecificChannelGroupFragment(specificChannelGroupCallback,mGroupId);
+                return mSpecificChannelGroupFragment;
+            case 4:
+                mMediaFragment = new MediaFragment(mediaCallback);
+                return mMediaFragment;
+            case 5:
+                mSpecificChannelFragment = new SpecificChannelFragment(mediaCallback1,mChannelId);
+                return mSpecificChannelFragment;
+            case 6:
                 mPlayerFragment = new PlayersFragment();
                 mRefer.put(0, mPlayerFragment);
                 return mPlayerFragment;
-            case 1:
-                SettingsFragment settingFragment1 = new SettingsFragment();
-                return settingFragment1;
             default:
                 return null;
             }
@@ -65,18 +199,33 @@ public class PlayerActivity extends FragmentActivity implements
 
         @Override
         public int getCount() {
-            return 2;
+            return count;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            String title = "Default";
+            String title = "";
             switch (position) {
             case 0:
-                title = getResources().getString(R.string.player);
+                title = getResources().getString(R.string.settings);
                 break;
             case 1:
-                title = getResources().getString(R.string.settings);
+                title = getResources().getString(R.string.channel_groups);
+                break;
+            case 2:
+                title = getResources().getString(R.string.all_channels);
+                break;
+            case 3:
+                title = getResources().getString(R.string.specific_channels);
+                break;
+            case 4:
+                title = getResources().getString(R.string.all_media);
+                break;
+            case 5:
+                title = getResources().getString(R.string.specific_media);
+                break;
+            case 6:
+                title = getResources().getString(R.string.player);
                 break;
             }
             return title;
@@ -88,32 +237,51 @@ public class PlayerActivity extends FragmentActivity implements
         mControl = control;
         if (mPlayerFragment == null)
             mPlayerFragment = (PlayersFragment) mRefer.get(0);
-        mPlayerFragment.setControl(mControl);
-        mPlayerFragment.showProgress(false,null);
-        mPlayerFragment.setEditText("28ed28ffc8e7438783732dc19fae6bbc");
+        if (mPlayerFragment != null){
+            mPlayerFragment.setControl(mControl);
+            mPlayerFragment.showProgress(false,null);
+        }
+        //mPlayerFragment.setEditText("28ed28ffc8e7438783732dc19fae6bbc");
     }
 
     @Override
     public void playerMessage(int what, int extra,String msg) {
-        if(what == Constants.Message.status.ordinal()){
-            mPlayerFragment.showProgress(true,msg);
+        if (mPlayerFragment == null) {
+            mPlayerFragment = (PlayersFragment) mRefer.get(0);
         }
-        else if(what == Constants.Message.progress.ordinal()){
-            mPlayerFragment.showProgressDialog(extra);
-        }
-        else{
-            if (mPlayerFragment == null) {
-                mPlayerFragment = (PlayersFragment) mRefer.get(0);
+        if(mPlayerFragment != null){
+            if(what == Constants.Message.status.ordinal()){
+                if(extra == Constants.PlayerState.completed.ordinal()){
+                    mPlayerFragment.playCompleted();
+                }else{
+                    mPlayerFragment.showProgress(true,msg);
+                }
             }
-            mPlayerFragment.hide();
-            mPlayerFragment.showProgress(false,null);
-            mPlayerFragment.showMessage(msg);
+            else if(what == Constants.Message.error.ordinal()){
+                mPlayerFragment.hide();
+                mPlayerFragment.showProgress(false,null);
+                mPlayerFragment.showMessage(msg);
+                
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPlayerFragment.playCompleted();
+                    }
+                });
+            }
+            else if(what == Constants.Message.progress.ordinal()){
+                mPlayerFragment.showProgress(false,null);
+                mPlayerFragment.showProgressDialog(extra);
+            }
+            else{
+                mPlayerFragment.hide();
+                mPlayerFragment.showProgress(false,null);
+                mPlayerFragment.showMessage(msg);
+            }
         }
     }
-
     @Override
-    public void onActivityResult(int requestCode, int resultCode,
-            Intent resultData) {
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
 
         if (requestCode == READ_REQUEST_CODE
                 && resultCode == Activity.RESULT_OK) {
@@ -122,12 +290,25 @@ public class PlayerActivity extends FragmentActivity implements
                 if (mPlayerFragment == null) {
                     mPlayerFragment = (PlayersFragment) mRefer.get(0);
                 }
-                mPlayerFragment.setEditText(mUri.toString());
-                mPlayerFragment.show();
+                if(mPlayerFragment!= null){
+                    if(URLUtil.isValidUrl(mUri.toString()) && URLUtil.isContentUrl(mUri.toString())){
+                        String[] proj = { MediaStore.Video.VideoColumns.DISPLAY_NAME };
+                        Cursor cursor = getContentResolver().query(mUri, proj, null, null, null);
+                        if (cursor != null && cursor.getCount() != 0) {
+                            cursor.moveToFirst();
+                            mPlayerFragment.setLocalfileName(cursor.getString(0));
+                        }
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                    }
+                        mPlayerFragment.setEditText(mUri.toString());
+                    mPlayerFragment.show();
+                }
                 if (mControl != null) {
-//                    mControl.setVideoUri(mUri);
-//                    mControl.play(null);
-                    mControl.play(mUri.toString(), null);
+                    //mControl.setVideoUri(mUri);
+                    //mControl.play(null);
+                    mControl.play(mUri.toString(), null);//local  file playback
                 } else
                     Log.e(getLocalClassName(), "Control is null");
             }
@@ -140,62 +321,52 @@ public class PlayerActivity extends FragmentActivity implements
     }
 
     @Override
-    public void onPageScrollStateChanged(int page) {
-        if (mPlayerFragment == null) {
-            mPlayerFragment = (PlayersFragment) mRefer.get(0);
-        }
-        mPlayerFragment.hideController();
-        if(page == 0){
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-            mPlayerFragment.show();
-        }else{
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-                mControl.pause();
-        }
-    }
+    public void onPageScrollStateChanged(int page) {}
 
     @Override
-    public void onPageScrolled(int page, float arg1, int arg2) {
-        if (mPlayerFragment == null) {
-            mPlayerFragment = (PlayersFragment) mRefer.get(0);
-        }
-        mPlayerFragment.hideController();
-        if(page == 0){
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-                mPlayerFragment.show();
-        }else{
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-                mControl.pause();
-        }
-    }
+    public void onPageScrolled(int page, float arg1, int arg2) {}
 
     @Override
     public void onPageSelected(int page) {
         if (mPlayerFragment == null) {
             mPlayerFragment = (PlayersFragment) mRefer.get(0);
         }
-        mPlayerFragment.hideController();
-        if(page == 0){
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-            mPlayerFragment.show();
-        }else{
-            if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
-                mControl.pause();
+        if (mPlayerFragment != null){
+            mPlayerFragment.hideController();
+            if(page == 7){
+                if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
+                    mPlayerFragment.show();
+            }else{
+                if(mPlayerFragment.getPlayerState() != PlayerState.stopped.ordinal())//stopped
+                    mControl.pause();
+            }
         }
+        getActionBar().setSelectedNavigationItem(page);
     }
-
-    /*@Override
-    public void fetchedEncodings(ArrayList<String> encoding) {
-        mPlayerFragment.showEncodingDialog(encoding);
-    }*/
 
     @Override
     public void playerPrepared(IPlayerControl control) {
         mControl = control;
         if (mPlayerFragment == null)
             mPlayerFragment = (PlayersFragment) mRefer.get(0);
-        mPlayerFragment.setControl(mControl);
-        mPlayerFragment.showProgress(false,null);
-        mPlayerFragment.show();
+        if (mPlayerFragment != null) {
+            mPlayerFragment.setControl(mControl);
+            mPlayerFragment.showProgress(false,null);
+            mPlayerFragment.show();
+            mPlayerFragment.showKeyboard(false);
+        }
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
     }
 }
