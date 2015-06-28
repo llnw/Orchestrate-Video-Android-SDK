203ad8e01261cf3b219650ab272fdba3ac125b0d
Nagaraju <nagaraju@rebaca.com>
2015-06-28 20:18:55 +0530

    Source for building sample app to use LL SDK using Android Studio
    Source for building sample app to use LL SDK using Android Studio

---
+++ SampleApplication/LLSDKTest_android_studio/app/src/main/java/com/example/ll/llsdktest/ListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.ll.llsdktest;
+
+
+
+
+        import java.util.ArrayList;
+        import com.limelight.videosdk.utility.Downloader;
+        import com.limelight.videosdk.utility.Thumbnail;
+        import android.content.Context;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.net.Uri;
+        import android.support.v4.app.FragmentActivity;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.BaseAdapter;
+        import android.widget.ImageView;
+        import android.widget.ProgressBar;
+        import android.widget.TextView;
+
+public class ListAdapter extends BaseAdapter {
+    private ArrayList<String> mTitleList;
+    private ArrayList<Uri> mUriList;
+    private ArrayList<String> mMediaIdList;
+    private FragmentActivity mFragmentActivity;
+    private Bitmap mLoadingBitmap = null;
+    private Downloader mDownloader = null;
+
+    class ViewHolder {
+        public TextView mTextView;
+        public ImageView mThumbnailImage;
+        public ProgressBar mProgress;
+    }
+    public ListAdapter(FragmentActivity mActivity) {
+        mFragmentActivity = mActivity;
+        mLoadingBitmap = BitmapFactory.decodeResource(mFragmentActivity.getResources(), android.R.drawable.ic_menu_gallery);
+        mDownloader = new Downloader(mFragmentActivity);
+    }
+    public ListAdapter(FragmentActivity mActivity,int type) {
+        mFragmentActivity = mActivity;
+        mLoadingBitmap = BitmapFactory.decodeResource(mFragmentActivity.getResources(), android.R.drawable.ic_menu_gallery);
+        mDownloader = new Downloader(mFragmentActivity);
+    }
+    public void setData(ArrayList<String> list, ArrayList<Uri> uri,ArrayList<String> mediaIds){
+        mUriList = uri;
+        mTitleList = list;
+        mMediaIdList = mediaIds;
+    }
+    public ListAdapter(FragmentActivity mActivity, ArrayList<String> mList, ArrayList<Uri> mUri) {
+        mTitleList = mList;
+        mFragmentActivity = mActivity;
+        mUriList = mUri;
+        mLoadingBitmap = BitmapFactory.decodeResource(mFragmentActivity.getResources(), android.R.drawable.ic_menu_gallery);
+        mDownloader = new Downloader(mFragmentActivity);
+    }
+    @Override
+    public int getCount() {
+        return mTitleList == null ? 0 : mTitleList.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return mTitleList == null ? null : mTitleList.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        if(rowView == null){
+            LayoutInflater inflater = (LayoutInflater) mFragmentActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.list_media, null);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.mTextView = (TextView) rowView.findViewById(R.id.text);
+            viewHolder.mThumbnailImage = (ImageView) rowView.findViewById(R.id.image);
+            viewHolder.mProgress = (ProgressBar) rowView.findViewById(R.id.progressThumbnail);
+            rowView.setTag(viewHolder);
+        }
+
+        final ViewHolder holder = (ViewHolder) rowView.getTag();
+        holder.mTextView.setText(mTitleList.get(position));
+        if(mUriList.get(position)!= null){
+            mDownloader.startDownload(mUriList.get(position).toString(), null, null, mMediaIdList.get(position),new Downloader.DownLoadCallback(){
+                @Override
+                public void onSuccess(String path) {
+                    Thumbnail t = new Thumbnail();
+                    t.loadBitmap(mFragmentActivity, path, holder.mThumbnailImage, null);
+                    holder.mProgress.setVisibility(View.GONE);
+                }
+                @Override
+                public void onError(Throwable ex) {
+                    holder.mThumbnailImage.setImageBitmap(mLoadingBitmap);
+                    holder.mProgress.setVisibility(View.GONE);
+                }
+                @Override
+                public void onProgress(int percentFinished) {
+                    holder.mThumbnailImage.setImageBitmap(mLoadingBitmap);
+                    holder.mProgress.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
+        return rowView;
+    }
+
+}
---
+++ SampleApplication/LLSDKTest_android_studio/app/src/main/java/com/example/ll/llsdktest/PlayersFragment.java
@@ -0,0 +1,140 @@
+package com.example.ll.llsdktest;
+
+
+        import com.limelight.videosdk.ContentService;
+        import com.limelight.videosdk.IPlayerControl;
+        import com.limelight.videosdk.PlayerSupportFragment;
+        import android.app.ProgressDialog;
+        import android.content.Context;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.util.Log;
+        import android.view.Gravity;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.inputmethod.InputMethodManager;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+public class PlayersFragment extends Fragment {
+
+    private PlayerSupportFragment mPlayer;
+    private ProgressDialog mProgress = null;
+    private ProgressDialog mProgressDialog = null;
+    private IPlayerControl mControl;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_player, container,false);
+
+        mProgress = new ProgressDialog(getActivity(), ProgressDialog.STYLE_SPINNER);
+        mProgress.setCanceledOnTouchOutside(false);
+        mProgressDialog = new ProgressDialog(getActivity());
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setMessage("Downloading. Please Wait...");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        showKeyboard(false);
+
+        mPlayer = new PlayerSupportFragment();
+        getChildFragmentManager().beginTransaction().add(R.id.videoLayout, mPlayer).addToBackStack(null).commitAllowingStateLoss();
+        return rootView;
+    }
+
+    public void play(ContentService svc, String mediaId) {
+        if (mControl != null) {
+            mControl.stop();
+            hide();
+            hideController();
+            mControl.play(mediaId, svc);//used with delivery
+        } else
+            Log.e(getActivity().getLocalClassName(), "Control is null");
+    }
+
+    public void setControl(IPlayerControl control) {
+        mControl = control;
+    }
+
+    public void showProgress(final boolean show, final String strMsg){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mProgress != null)
+                    if(show){
+                        mProgress.setMessage(strMsg);
+                        mProgress.show();
+                    }
+                    else{
+                        mProgress.hide();
+                    }
+            }
+        });
+    }
+
+    public void showProgressDialog(final int percent){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mProgressDialog != null){
+                    if(!mProgressDialog.isShowing() && percent==0){
+                        mProgressDialog.show();
+                    }
+                    if(percent != 100){
+                        mProgressDialog.setProgress(percent);
+                    }else{
+                        mProgressDialog.dismiss();
+                    }
+                }
+            }
+        });
+    }
+
+    public void showMessage(final String msg){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast toast = new Toast(getActivity());
+                toast.setGravity(Gravity.TOP, 0, 300);
+                toast.setDuration(Toast.LENGTH_LONG);
+                LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.toast, null);
+                TextView tv = (TextView)view.findViewById(R.id.text);
+                tv.setText(msg);
+                toast.setView(view);
+                toast.show();
+            }
+        });
+    }
+
+    public void show() {
+        getChildFragmentManager().beginTransaction().show(mPlayer).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+    public void hide() {
+        getChildFragmentManager().beginTransaction().hide(mPlayer).addToBackStack(null).commitAllowingStateLoss();
+    }
+
+    public void hideController(){
+        mPlayer.hideMediaController();
+    }
+
+    public int getPlayerState(){
+        return mPlayer.getCurrentPlayState();
+    }
+
+    public void showKeyboard(boolean show) {
+        // Check if no view has focus:
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(show){
+                inputManager.showSoftInput(view, InputMethodManager.HIDE_NOT_ALWAYS);
+            }else{
+                inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                view.clearFocus();
+            }
+        }
+    }
+}
